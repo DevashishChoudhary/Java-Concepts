95eae5ebbf01d29cb6a4b4e8224f68042dadc465
DevashishChoudhary <46700867+DevashishChoudhary@users.noreply.github.com>
2020-05-16 12:17:07 +0530

    Updated to user define formate

--- Operators.java
+++ Operators.java
@@ -29,12 +29,15 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Enter the meal cost --> ");
         double meal_cost = scanner.nextDouble();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+        System.out.println("Enter the tip amount --> ");
         int tip_percent = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+        System.out.println("Enter the tax percentage --> ");
         int tax_percent = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
